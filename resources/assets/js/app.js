2a3fa8df142c9a5b8a92b95258e777cdacab7cdf
joma74 <dev-mails@gmx.net>
2018-01-18 23:55:34 +0100

    reafctor(app): moved prototype to laravel

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,51 @@
-
+import Vue from "vue";
+import sample from'./data';
 /**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
+ * @type {Vue}
  */
-
-require('./bootstrap');
-
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
+var app = new Vue({
+  el: "#app",
+  data: {
+    headerImageStyle: {
+      "background-image": "url('images/header.jpg')"
+    },
+    contracted: true,
+    modalOpen: false,
+    title: sample.title,
+    address: sample.address,
+    about: sample.about,
+    amenities: sample.amenities,
+    prices: sample.prices
+  },
+  beforeCreate: function() {
+    console.log("vue-lcy: beforeCreate -> " + this.title);
+  },
+  created: function() {
+    console.log("vue-lcy: created -> " + this.title);
+    document.addEventListener("keyup", this.escapeKeyListener /* <- This Is Genius !:) */);
+  },
+  methods: {
+    /**
+     * @param {KeyboardEvent} evt
+     */
+    escapeKeyListener: function(evt) {
+      if (evt.keyCode === 27 && this.modalOpen /* <- This Is Genius !:) */) {
+        this.modalOpen = false;
+      }
+    }
+  },
+  watch: {
+    modalOpen: function() {
+      var className = "modal-open";
+      if (this.modalOpen) {
+        document.body.classList.add(className);
+      } else {
+        document.body.classList.remove(className);
+      }
+    }
+  },
+  destroyed: function() {
+    console.log("vue-lcy: destroyed -> " + this.title);
+    document.removeEventListener("keyup", this.escapeKeyListener);
+  }
 });
---
+++ resources/views/app.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico" />
+	<title>vuebnb</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">
+	<link rel="stylesheet" href="https://unpkg.com/open-sans-all/css/open-sans.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+</head>
+
+<body>
+	<div id="toolbar">
+		<img class="icon" src="../images/logo.png">
+		<h1>vuebnb</h1>
+	</div>
+	<div id="app">
+		<div class="header">
+			<div class="header-img" :style="headerImageStyle">
+				<button class="view-photos" @click="modalOpen = true">View Photos</button>
+			</div>
+		</div>
+		<div class="container">
+			<div class="heading">
+				<h1>@{{ title }}</h1>
+				<p>@{{ address }}</p>
+			</div>
+			<hr>
+			<div class="about">
+				<h3>About this listing</h3>
+				<p :class="{ contracted: contracted }">@{{ about }}</p>
+				<button v-if="contracted" class="more" @click="contracted = false">+ More</button>
+			</div>
+			<div class="lists">
+				<div class="amenities list">
+					<div class="title">
+						<strong>Amenities</strong>
+					</div>
+					<div class="content">
+						<div class="list-item" v-for="amenity in amenities">
+							<i class="fa fa-lg" :class="amenity.icon"></i>
+							<span>@{{ amenity.title }}</span>
+						</div>
+					</div>
+				</div>
+				<div class="prices list">
+					<div class="title">
+						<strong>Prices</strong>
+					</div>
+					<div class="content">
+						<div class="list-item" v-for="price in prices">
+							@{{ price.title }}:
+							<strong>@{{ price.value }}</strong>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="modal" :class="{ show: modalOpen }">
+			<button v-on:click="modalOpen = false" class="modal-close">
+					&times;
+			</button>
+		</div>
+	</div>
+	<script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+</body>
+
+</html>
