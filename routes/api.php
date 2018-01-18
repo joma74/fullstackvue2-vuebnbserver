be86967780cf81a64562a302dbdcef81a2d7bd99
joma74 <dev-mails@gmx.net>
2018-01-18 02:00:07 +0100

    chore(migrate) commit before going to new repo

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Listing;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('listing/{listing}', 'ListingController@get_listing_api');
