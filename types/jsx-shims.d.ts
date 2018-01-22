70e74affc98013497ddae8788ee3d6631247775c
joma74 <dev-mails@gmx.net>
2018-01-23 00:17:04 +0100

    chore(tsint): Added shims for Vue and JSX

---
+++ types/jsx-shims.d.ts
@@ -0,0 +1,11 @@
+// See https://medium.com/@hayavuk/using-jsx-with-vue-js-and-typescript-d6963e44de48
+import Vue, { VNode } from "vue";
+declare global {
+  declare namespace JSX {
+    interface Element extends VNode { }
+    interface ElementClass extends Vue { }
+    interface IntrinsicElements {
+      [elem: string]: any;
+    }
+  }
+}
