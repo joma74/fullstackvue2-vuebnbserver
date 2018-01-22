90cff9b67e7f0ba4270b459816c93e30a9bcb39c
joma74 <dev-mails@gmx.net>
2018-01-23 00:42:08 +0100

    refactor: extract carousel-control into own SFC

---
+++ resources/assets/js/components/CarouselControl.vue
@@ -0,0 +1,42 @@
+<template>
+  <i :class="classes" @click="clicked"></i>
+</template>
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    dir: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    clicked: function() {
+      if ("left" === this.dir) {
+        this.$emit("change-image", -1);
+      } else {
+        this.$emit("change-image", +1);
+      }
+    }
+  },
+  computed: {
+    classes() {
+      return "carousel-control fa fa-2x fa-chevron-" + this.dir;
+    }
+  }
+});
+</script>
+<style>
+.carousel-control {
+  padding: 1rem;
+  color: #ffffff;
+  opacity: 0.85;
+}
+
+@media (min-width: 744px) {
+  .carousel-control {
+    font-size: 3rem;
+  }
+}
+</style>
--- resources/assets/js/components/ImageCarousel.vue
+++ resources/assets/js/components/ImageCarousel.vue
@@ -15,32 +15,11 @@
 </template>
 <script>
 import Vue from "vue";
+import CarouselControl from "../components/CarouselControl.vue";
 
 export default Vue.extend({
   components: {
-    "carousel-control": {
-      template: `<i :class="classes" @click="clicked"></i>`,
-      props: {
-        dir: {
-          type: String,
-          required: true
-        }
-      },
-      methods: {
-        clicked: function() {
-          if ("left" === this.dir) {
-            this.$emit("change-image", -1);
-          } else {
-            this.$emit("change-image", +1);
-          }
-        }
-      },
-      computed: {
-        classes() {
-          return "carousel-control fa fa-2x fa-chevron-" + this.dir;
-        }
-      }
-    }
+    CarouselControl
   },
   props: {
     images: {
@@ -96,17 +75,5 @@ export default Vue.extend({
   width: 100%;
   display: flex;
   justify-content: space-between;
-}
-
-.carousel-control {
-  padding: 1rem;
-  color: #ffffff;
-  opacity: 0.85;
-}
-
-@media (min-width: 744px) {
-  .carousel-control {
-    font-size: 3rem;
-  }
 }
 </style>
