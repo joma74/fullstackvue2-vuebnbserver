bd6bd50aaac406ce07a8d1e0a6eaa2079809001a
joma74 <dev-mails@gmx.net>
2018-01-27 21:04:27 +0100

    feat(store): initial setup

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./components/App.vue";
 import router from "./router";
+import store from "./store";
 import "./../css/style.css";
 
 /**
@@ -9,5 +10,7 @@ import "./../css/style.css";
 var app = new Vue({
   el: "#app",
   render: h => h(App),
-  router
+  router,
+  store
 });
+
