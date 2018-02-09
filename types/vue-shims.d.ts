4e80fc5077806ebbe79cce41696b03e93f8f0690
joma74 <dev-mails@gmx.net>
2018-02-09 01:26:19 +0100

    maintain(tsconfig): vue store+router unification + windows
    Seems that either vuex's or router's vue.d.ts is available; moved both
    into vue shim.
    
    Annonced server side window variabel

--- types/vue-shims.d.ts
+++ types/vue-shims.d.ts
@@ -2,3 +2,47 @@ declare module "*.vue" {
     import Vue from 'vue';
     export default Vue;
 }
+
+/**
+ * Augment the typings of Vue.js
+ */
+
+import Vue from "vue";
+import VueRouter, { Route, RawLocation, NavigationGuard } from "vue-router/types/index";
+
+declare module "vue/types/vue" {
+  interface Vue {
+    $router: VueRouter;
+    $route: Route;
+  }
+}
+
+declare module "vue/types/options" {
+  interface ComponentOptions<V extends Vue> {
+    router?: VueRouter;
+    beforeRouteEnter?: NavigationGuard;
+    beforeRouteLeave?: NavigationGuard;
+    beforeRouteUpdate?: NavigationGuard;
+  }
+}
+
+
+/**
+ * Augment the typings of Vue.js
+ */
+
+import Vue from "vue";
+import { ComponentOptions } from "vue";
+import { Store } from "vuex/types/index";
+
+declare module "vue/types/vue" {
+  interface Vue {
+    $store: Store<any>;
+  }
+}
+
+declare module "vue/types/options" {
+  interface ComponentOptions<V extends Vue> {
+    store?: Store<any>;
+  }
+}
