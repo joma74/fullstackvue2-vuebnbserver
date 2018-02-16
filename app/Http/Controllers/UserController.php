ad407dfda2ea59061b86d301b0128af98a7723fd
joma74 <dev-mails@gmx.net>
2018-02-16 22:26:20 +0100

    feat(auth): Added passport to laravel app

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,17 +9,17 @@ use Illuminate\Support\Facades\Input;
 class UserController extends Controller
 {
     public function toggle_saved(){
-    $id = Input::get('id');
-    $user = Auth::user();
-    $saved = $user->saved;
-    $key = array_search($id, $saved);
-    if ($key === FALSE) {
-        array_push($saved, $id);
-    } else {
-        array_splice($saved, $key, 1);
-    }
-    $user->saved = $saved;
-    $user->save();
-    return response()->json();
+        $id = Input::get('id');
+        $user = Auth::user();
+        $saved = $user->saved;
+        $key = array_search($id, $saved);
+        if ($key === FALSE) {
+            array_push($saved, $id);
+        } else {
+            array_splice($saved, $key, 1);
+        }
+        $user->saved = $saved;
+        $user->save();
+        return response()->json();
   }
 }
